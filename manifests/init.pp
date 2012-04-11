fae8d6856c94fabb3389f6c8904f5ecbdf8c6505
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-04-11 14:02:50 +0200

    Fix several issues detected by puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,44 +3,44 @@ class rsyslog {
 		file { "$name":
 			owner   => root,
 			group   => root,
-			mode    => 0644,
-			alias   => "rsyslog.conf",
+			mode    => '0644',
+			alias   => 'rsyslog.conf',
 			content => template("rsyslog/${::lsbdistcodename}/etc/rsyslog-${config}.conf.erb"),
-			notify  => Service["rsyslog"],
-			require => Package["rsyslog-relp"],
+			notify  => Service['rsyslog'],
+			require => Package['rsyslog-relp'],
 		}
 	}
 
-	rsyslog::config { "/etc/rsyslog.conf":
-		config => "client",
+	rsyslog::config { '/etc/rsyslog.conf':
+		config => 'client',
 		host   => hiera('host'),
 	}
 
-	if $::lsbdistid == "Ubuntu" {
-		file { "/etc/rsyslog.d/50-default.conf":
+	if $::lsbdistid == 'Ubuntu' {
+		file { '/etc/rsyslog.d/50-default.conf':
 			ensure => absent,
 		}
 	}
 
-	file { "/var/spool/rsyslog":
+	file { '/var/spool/rsyslog':
 		ensure => directory,
 		owner  => root,
 		group  => root,
-		mode   => 0755,
+		mode   => '0755',
 	}
 
-	package { "rsyslog-relp":
+	package { 'rsyslog-relp':
 		ensure => present,
 	}
 
-	service { "rsyslog":
-		enable     => true,
+	service { 'rsyslog':
 		ensure     => running,
+		enable     => true,
 		hasrestart => true,
 		hasstatus  => true,
 		require    => [
-			File["rsyslog.conf"],
-			Package["rsyslog-relp"]
+			File['rsyslog.conf'],
+			Package['rsyslog-relp']
 		],
 	}
 }
--- manifests/master.pp
+++ manifests/master.pp
@@ -1,27 +1,27 @@
 class rsyslog::master inherits rsyslog {
-	file { "/etc/logrotate.d/rsyslog":
+	file { '/etc/logrotate.d/rsyslog':
 		owner   => root,
 		group   => root,
-		mode    => 0644,
-		source  => "puppet:///modules/rsyslog/common/etc/logrotate.d/rsyslog",
+		mode    => '0644',
+		source  => 'puppet:///modules/rsyslog/common/etc/logrotate.d/rsyslog',
 		require => [
-			File["rsyslog.conf"],
-			Package["rsyslog-relp"]
+			File['rsyslog.conf'],
+			Package['rsyslog-relp']
 		],
 	}
 
-	Rsyslog::Config["/etc/rsyslog.conf"] {
-		config  => "server",
+	Rsyslog::Config['/etc/rsyslog.conf'] {
+		config  => 'server',
 	}
 
-	file { "/var/log/rsyslog":
+	file { '/var/log/rsyslog':
 		ensure => directory,
 		owner  => root,
 		group  => root,
-		mode   => 0755,
+		mode   => '0755',
 	}
 
-	File ["/var/spool/rsyslog"] {
+	File ['/var/spool/rsyslog'] {
 		force  => true,
 		ensure => absent,
 	}
